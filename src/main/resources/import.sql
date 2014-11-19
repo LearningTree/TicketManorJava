a82453ec6b27ba269f7508af4240fad4fc90f2db
Ian Darwin <ian@darwinsys.com>
2014-11-19 09:28:56 -0500

    Set up JPA: gets as far as trying to login to the database
    So the JPA mappings we have so far are at least syntactically sound.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+-- import.sql for TicketManor project
+
+insert into venues(id, name) values(-1, 'Carnegie Hall');
+
+insert into acts(id, name) values(-1, 'Rush');
+
+insert into people(id, firstname, lastname) values(-1, 'System', 'Administrator');
+insert into members(id) values(-1);
