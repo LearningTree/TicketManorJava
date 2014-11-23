9e69905f8db1723df2259983d1eee8197b1c1d4b
Ian Darwin <ian@darwinsys.com>
2014-11-23 14:46:12 -0500

    Comment--only change

--- src/main/java/com/ticketmanor/model/Member.java
+++ src/main/java/com/ticketmanor/model/Member.java
@@ -10,6 +10,6 @@ import javax.persistence.*;
 public class Member extends Person  {
 	// the id property is inherited
 
-	/** The profile photo. A full-blown UI should support uploading this. */
+	/** The profile photo. A full implementation of the UI will support uploading this. */
 	private byte[] profilePhoto;
 }
