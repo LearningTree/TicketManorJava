1c18d0afe427c5e8157fb17b6f487b70e8d9b734
Ian Darwin <ian@darwinsys.com>
2014-12-22 15:42:46 -0500

    Formatting-only

--- src/main/java/com/ticketmanor/model/Person.java
+++ src/main/java/com/ticketmanor/model/Person.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotNull;
  * Person - One individual person who has some interaction with the system.
  */
 @Entity @Table(name="People")
-public class Person   {
+public class Person {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	long id;
 	
