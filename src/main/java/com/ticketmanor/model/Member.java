65327125514fefac4e90f93b7a68e8742043cd71
Ian Darwin <ian@darwinsys.com>
2014-12-22 15:51:30 -0500

    Revert previous

--- src/main/java/com/ticketmanor/model/Member.java
+++ src/main/java/com/ticketmanor/model/Member.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
  * Member - A Person who has signed up for the site.
  */
 @Entity
+@Table(name="Members")
 public class Member extends Person  {
 	// the id property is inherited
 
