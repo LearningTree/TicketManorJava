275330c1ea1376829e799f3c8c8cf6db4a5fa554
Ian Darwin <ian@darwinsys.com>
2014-12-23 21:07:08 -0500

    Case-sensitive fields? In SQL? Seriously?

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,5 +4,5 @@ insert into venues(id, name) values(-1, 'Carnegie Hall');
 
 insert into acts(id, name, yearFounded) values(-1, 'Rush', 1980);
 
-insert into people(id, firstname, lastname) values(-1, 'System', 'Administrator');
+insert into people(id, firstName, lastName) values(-1, 'System', 'Administrator');
 insert into members(id) values(-1);
