a2c22a3efa1051c21b42472e1367888b100e27cb
Ian Darwin <ian@darwinsys.com>
2015-01-16 10:34:53 -0500

    More exacting date format needed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,7 +8,7 @@ insert into Acts(id, title, year) values(-2, 'Foo Fish', 2020);
 insert into Acts (id, title, type, year) values (-3, 'Star Wars Bloopers', 0, 2001);
 insert into Movie (id, director) values (-3, 'George Lucas');
 
-insert into Events (id, what_id, date_time, where_id) values (-1, -3, '2016-06-22T14:00', null);
+insert into Events (id, what_id, date_time, where_id) values (-1, -3, '2016-06-22T14:00:00.000', null);
 
 insert into people(id, firstName, lastName) values(-1, 'System', 'Administrator');
 insert into members(id) values(-1);
