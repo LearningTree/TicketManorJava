7a1ca59abd8c0d6a113339bc893a681152b0d494
Ian Darwin <ian@darwinsys.com>
2015-01-16 21:39:58 -0500

    Comment empty CLASS

--- src/main/java/com/ticketmanor/rest/RestApplication.java
+++ src/main/java/com/ticketmanor/rest/RestApplication.java
@@ -6,5 +6,5 @@ import javax.ws.rs.core.Application;
 /** This class is needed to enable REST and to specify the base path for REST services */
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
-
+	// Empty
 }
