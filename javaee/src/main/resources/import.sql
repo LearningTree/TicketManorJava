f3086725f69f11801605c6ae5f33d0fcc2616771
Ian Darwin <ian@darwinsys.com>
2015-01-25 12:46:16 -0500

    Sync with table names

--- javaee/src/main/resources/import.sql
+++ javaee/src/main/resources/import.sql
@@ -7,9 +7,9 @@ insert into acts(id, title, year) values(-1, 'Rush', 1980);
 insert into acts(id, title, year) values(-2, 'Foo Fish', 2020);
 
 insert into acts (id, title, type, year) values (-3, 'Star Wars Bloopers', 0, 2001);
-insert into movie (id, director) values (-3, 'George Lucas');
+insert into movies (id, director) values (-3, 'George Lucas');
 
-insert into events (id, what_id, date_time, where_id) values (-1, -3, '2016-06-22T14:00:00.000', null);
+insert into events (id, what_id, date_time, where_id) values (-1, -3, '2016-06-22 14:00:00', null);
 
 insert into people(id, firstName, lastName) values(-1, 'System', 'Administrator');
 insert into members(id) values(-1);
