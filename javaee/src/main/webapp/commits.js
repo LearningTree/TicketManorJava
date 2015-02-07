8992d75f7044788b59c26e5de624e262c65ac67d
Ian Darwin <ian@darwinsys.com>
2015-02-06 23:49:57 -0500

    trivia

--- javaee/src/main/webapp/commits.js
+++ javaee/src/main/webapp/commits.js
@@ -1,13 +1,13 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  // put any routes here
 });
 
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
     return Ember.$.getJSON('https://api.github.com/repos/LearningTree/TicketManorJava/commits').then(function(data) {
-      return data.splice(0, 3);
+      return data.splice(0, 5);
     });
   }
 });
