2e10696e2a35a7410096438db5d5b7ec2bd2c440
Ian Darwin <ian@darwinsys.com>
2015-02-14 18:19:46 -0500

    Add base64 authentication.

--- javaee-client/src/main/java/com/ticketmanor/rest/client/JaxRsClientMain.java
+++ javaee-client/src/main/java/com/ticketmanor/rest/client/JaxRsClientMain.java
@@ -1,15 +1,20 @@
 package com.ticketmanor.rest.client;
 
+import java.util.Base64;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
-//import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.jackson.JacksonFeature;
 
+
+//import org.glassfish.jersey.jackson.JacksonFeature;
 import com.ticketmanor.model.Event;
 
-/** This will become an example of the JAX-RS 2.0 Client API.
+/** 
+ * Example client the JAX-RS 2.0 Client API.
  * It has a dependency on Jersey (which is in our POM file now).
  * It is confused by some fields in the returned JSON data...
  * @author Ian Darwin
@@ -18,15 +23,25 @@ public class JaxRsClientMain {
 
 	private static final String URL = 
 		"http://localhost:8080/ticketmanor/rest/events/-1";
+	private static final String AUTH_HEADER = "Authorization";
+	static String auth_value;
 
 	public static void main(String[] args) {
+		auth_value = encodeAuth("robin", "secret54321");
 		process();
 	}
+	
+	private static String encodeAuth(String userName, String password) {
+		String cred = userName + ":" + password;
+		return new String(Base64.getEncoder().encode(cred.getBytes()));
+	}
+	
 	public static Event process() {
 		Client cl = ClientBuilder.newClient();
-		//cl.register(JacksonFeature.class);
+		cl.register(JacksonFeature.class);
 		WebTarget target = cl.target(URL);
-		Event e = target.request(MediaType.APPLICATION_JSON).get(Event.class);
+		Event e = target.request(MediaType.APPLICATION_JSON)
+				.header(AUTH_HEADER, auth_value).get(Event.class);
 		System.out.println("Got an event: " + e);
 		return e;
 	}
@@ -35,6 +50,7 @@ public class JaxRsClientMain {
 		Event e = ClientBuilder.newClient()
 				.target(URL)
 				.request(MediaType.APPLICATION_JSON)
+				.header(AUTH_HEADER, "bar")
 				.get(Event.class);
 		System.out.println("Got an event: " + e);
 		return e;
