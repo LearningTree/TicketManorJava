5ed617ef229e6e5631875c9eac8b2ca750b9e0a3
Ian Darwin <ian@darwinsys.com>
2018-01-25 10:48:09 -0500

    Put these into SQL, not code

---
+++ ticketmanor-ee/sql/populate-venues.sql
@@ -0,0 +1,8 @@
+insert into venues(id, city, country, lat, lng, name, provstate, streetaddress) values ( 6, 'Flippingdon', null, null, null, 'United Centre', null, null);
+insert into venues(id, city, country, lat, lng, name, provstate, streetaddress) values ( 7, 'London', 'UK', null, null, 'London Palladium', null, null);
+insert into venues(id, city, country, lat, lng, name, provstate, streetaddress) values ( 1, 'New York', 'US', 40.764938, -73.979897, 'Carnegie Hall', 'NY', '881 Seventh Avenue');
+insert into venues(id, city, country, lat, lng, name, provstate, streetaddress) values ( 4, 'San Francisco', 'US', null, null, 'Candlestick Park', null, null);
+insert into venues(id, city, country, lat, lng, name, provstate, streetaddress) values ( 2, 'Toronto', 'CA', 43.646596, -79.386413, 'Roy Thompson Hall', 'ON', '60 Simcoe Street');
+insert into venues(id, city, country, lat, lng, name, provstate, streetaddress) values ( 5, 'Toronto', 'CA', null, null, 'Maple Leaf Gardens', null, null);
+insert into venues(id, city, country, lat, lng, name, provstate, streetaddress) values ( 3, 'Toronto', 'CA', null, null, 'Rogers Stadium', null, null);
+insert into venues(id, city, country, lat, lng, name, provstate, streetaddress) values ( 8, 'Toronto', 'CA', null, null, 'Air Canada Centre', null, null);
