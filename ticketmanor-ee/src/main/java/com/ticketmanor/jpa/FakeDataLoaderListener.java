98bdb1a3890ccf399952ef63511c273df51545f1
Ian Darwin <ian@darwinsys.com>
2018-01-26 12:08:02 -0500

    Make LoaderListener with @WebListener

--- ticketmanor-ee/src/main/java/com/ticketmanor/jpa/FakeDataLoaderListener.java
+++ ticketmanor-ee/src/main/java/com/ticketmanor/jpa/FakeDataLoaderListener.java
@@ -3,20 +3,20 @@ package com.ticketmanor.jpa;
 import javax.ejb.EJB;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 /** Load some fake data. Tried to make this a Singleton EJB
  * but WildFly didn't run its @PostConstruct method.
- * If you convert it back to a singleton, remember to unplug
- * it from web.xml!
  * @author Ian Darwin
  */
+@WebListener
 public class FakeDataLoaderListener implements ServletContextListener {
 
 	@EJB FakeDataLoaderEjb ejb;
 	
 	@Override
 	public void contextInitialized(ServletContextEvent ctx) {
-		System.out.println("FakeEventLoader.contextInitialized()");
+		System.out.println("FakeEventLoader: servlet context initialized()");
 		ejb.loadFakeData();
 	}
 
