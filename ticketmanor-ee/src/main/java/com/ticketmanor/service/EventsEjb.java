62418d9d402f77b93f87a10d56706ee49bff0e1c
Ian Darwin <ian@darwinsys.com>
2021-07-12 13:54:21 -0400

    Generate fake data, keep it in memory to reduce overhead

--- ticketmanor-ee/src/main/java/com/ticketmanor/service/EventsEjb.java
+++ ticketmanor-ee/src/main/java/com/ticketmanor/service/EventsEjb.java
@@ -2,29 +2,45 @@ package com.ticketmanor.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Vector;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Local;
 import javax.ejb.Remote;
-import javax.ejb.Stateless;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import com.ticketmanor.model.Act;
+import com.ticketmanor.model.ActType;
 import com.ticketmanor.model.Event;
 import com.ticketmanor.model.Location;
+import com.ticketmanor.model.Venue;
 
-/** Slightly mis-named; should be ConcertsEjb */
-@Stateless
+/** 
+ * Slightly mis-named; should be ConcertsEjb
+ * Generates fake data and keeps it in an in-memory List<Event> to reduce overhead.
+ */
+@Singleton
+@Startup
 @Local @Remote
 @Path("eventsEjb")
 public class EventsEjb {
+    private static final long serialVersionUID = +531574255525675305L;
+
 	@PersistenceContext EntityManager em;
-	
+
+	private static List<Event> allEvents;
+
 	/**
 	 * Get All events
 	 * @return A list of all Events
@@ -34,6 +50,41 @@ public class EventsEjb {
 				em.createQuery("from Event e", Event.class);
 		return query.getResultList();
 	}
+
+    @PostConstruct
+    public void started() {
+        System.out.println("EventsEjb started()");
+		fakeEvents();
+    }
+
+	/**
+	 * Make up new fake events; scheduled to run daily before midnight.
+	 * Switches "allEvents" atomically in case one is viewing while this runs.
+	 * Uses Vector as necessary for thread safety.
+	 */
+    @Schedule(hour="23", minute="55", persistent=false)
+    public void fakeEvents() {
+		System.out.println("EventsEjb::fakeEvents called");
+		Act firstAct = em.find(Act.class, 1L); // must exist
+		List<Act> sportsTeams = em.createQuery("from Act act where act.type = ?1", Act.class).
+				setParameter(1, ActType.SPORTS).
+				getResultList();
+		
+		List<Venue> allVenues = em.createQuery("from Venue v", Venue.class).getResultList();
+		List<Event> newAll = new Vector<>();
+		LocalDateTime start = LocalDateTime.now().withHour(19).withMinute(0);
+		int nTeams = 0, nVenues=0;
+		Event ev = new Event(firstAct, start.plusDays(i), allVenues.get(0));
+		newAll.add(ev);
+		for (int i = 0; i < 20; i++) {
+			nTeams++; nTeams %= sportsTeams.size();
+			nVenues++; nVenues %= allVenues.size();
+			Event sp = new Event(sportsTeams.get(nTeams), start.plusDays(i), allVenues.get(nVenues));
+			newAll.add(sp);
+		}
+		allEvents = newAll;
+	}
+
 	
 	/** 
 	 * Get a list of Events on the given date
@@ -41,10 +92,11 @@ public class EventsEjb {
 	 * @return a list of Events for that date
 	 */
 	public List<Event> getEventsForDate(LocalDate selectedDate) {
-		final TypedQuery<Event> q = 
-				em.createQuery("from Event e where e.date like " + selectedDate,
-				Event.class);
-		return q.getResultList();
+		final List<Event> l = new Vector<>();
+		return allEvents
+			.stream()
+			.filter(evt->evt.getDate().equals(selectedDate))
+			.toList();
 	}
 	
 	/**
@@ -53,13 +105,18 @@ public class EventsEjb {
 	 * @return A list of Events over the next n days
 	 */
 	public List<Event> getEventsNextNDays(int nDays) {
-		LocalDateTime start = LocalDateTime.now();
-		LocalDateTime end = LocalDateTime.from(start).plusDays(nDays);
-		return em
-				.createQuery("from Event e where e.date >= ?1 AND e.date <= ?2", Event.class)
-				.setParameter(1, start)
-				.setParameter(2, end)
-				.getResultList();
+		return allEvents
+			.stream()
+			.filter(evt -> dateInRange(evt, nDays))
+			.toList();
+	}
+
+	boolean dateInRange(Event evt, int nDays) {
+		LocalDate start = LocalDate.now();
+		LocalDate end = LocalDate.from(start).plusDays(nDays);
+		long evtDay = evt.getDate().toLocalDate().toEpochDay();
+
+		return evtDay >= start.toEpochDay() && evtDay <= end.toEpochDay();
 	}
 
 	/**
@@ -80,7 +137,7 @@ public class EventsEjb {
 	 */
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public void addEvent(Event event) {
-		em.persist(event);
+		allEvents.add(event);
 	}
 
 	/**
@@ -89,6 +146,6 @@ public class EventsEjb {
 	 */
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public void deleteEvent(Event event) {
-		em.remove(event);
+		allEvents.remove(event);
 	}
 }
